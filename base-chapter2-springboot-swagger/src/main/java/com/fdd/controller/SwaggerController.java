ffb723195c0010b0d0a13e2b5f2511be8872b05f
fengdongdongwsn <1183373614@qq.com>
2021-03-05 13:47:54 +0800

    整合了swagger2

---
+++ base-chapter2-springboot-swagger/src/main/java/com/fdd/controller/SwaggerController.java
@@ -0,0 +1,40 @@
+package com.fdd.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class SwaggerController {
+
+    @PostMapping(value = "/add")
+    @ApiOperation(value = "添加方法", notes = "添加方法")
+    public String add(String  name) {
+        return "add "+ name + " success";
+    }
+
+    @PutMapping(value = "/update")
+    @ApiOperation(value = "更新评估任务", notes = "更新评估任务")
+    public String update(String name) {
+        return "success";
+    }
+
+    @DeleteMapping(value = "/delete")
+    @ApiOperation(value = "删除评估任务", notes = "删除评估任务")
+    public String delete(String name) {
+        return "success";
+    }
+
+    @GetMapping(value = "/selectone")
+    @ApiOperation(value = "按照名字查询", notes = "按照名字查询")
+    public String selectOne(String name) {
+        return ""+name;
+    }
+
+    @GetMapping(value = "/selectAll")
+    @ApiOperation(value = "查询所有", notes = "查询所有")
+    public String selectAll() {
+        return "json";
+    }
+
+}
